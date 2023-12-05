e7f3c5b46cd244c293751b2f73abda81505e0cde
Prathmeshm1206 <144794295+Prathmeshm1206@users.noreply.github.com>
2023-12-05 16:33:15 +0000

    new

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553;
+let accountEmail = "omkar@gmail.com"
+var accountPassword="12345"
+
+accountCity = "jaypur"
+
+//accountId = 2
+let accountState;
+accountEmail = "hc@gmail.com"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+Prefer not to use var
+because its block scope and funtional scope
+
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+//"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3)  //error  //it will work on browser bcoz browser contain javaScript engine
+
+console.log(3+3)  //code readibility  should be high
+
+console.log("hitesh")
+
+let = "hitesh"
+let = 18
+let isLogged = false
+
+// number => 2 to power 53
+// bigint => for big number
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => let state;,let state = null
+// symbol => unique
+
+// object
+
+console.log(typeof null);  //=> type of null object
