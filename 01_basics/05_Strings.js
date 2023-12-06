dc1042feb2202a75d65d7d7896bf20678e4e2422
Prathmeshm1206 <144794295+Prathmeshm1206@users.noreply.github.com>
2023-12-06 18:05:26 +0000

    Math and Num

--- 01_basics/05_Strings.js
+++ 01_basics/05_Strings.js
@@ -6,7 +6,7 @@ console.log(name + repoCount + "value");
 console.log(`Hello my name is ${name} and my repo is ${repoCount}`);
 console.log(`hey myself ${name} and my repo count is ${repoCount} `);
 
-const gameName = new String('hiteshhhc')
+const gameName = new String('hitesh-h-hc')   //String is object which stores key value pair
 
 console.log(gameName[0]);
 console.log(gameName.__proto__); // contains objects
@@ -27,3 +27,11 @@ console.log(anotherString)
 const newStringOne = "    hitesh   "
 console.log(newStringOne);
 console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hite%20mane"
+
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('hitesh')); //true
+console.log(gameName.split('-'));  //split from - 
---
+++ 01_basics/06_Nums_and_Maths.js
@@ -0,0 +1,43 @@
+const score = 400
+
+const balance = new Number(100)
+//console.log(balance);
+
+// console.log(balance.toString());  //converting into strings
+// console.log(balance.toFixed(2));  // it is used for calculating gst
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(3));  //priority before decimal points
+
+const hundreds = 10000000
+//console.log(hundreds.toLocaleString('en-IN'));  //convering 1,00,00,000
+
+/// ++++++++++++++++++++++++++++++++  Maths  ++++++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-3));  => +ve
+ console.log(Math.round(4.3));  //=>4
+// console.log(Math.ceil(4.3));  =>5
+// console.log(Math.floor(4.3)); =>4
+
+// console.log(Math.min(4, 3, 6, 8));
+console.log(Math.floor(Math.random()*10)+1);  // => 1 // random => values comes inbetween 0 and 1
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1))+min)
+
+
+
+
+
+
+
+
+
+
+
+
