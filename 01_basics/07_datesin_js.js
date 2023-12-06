01618d8b47bb8f639779ccb41919c31c313ba78a
Prathmeshm1206 <144794295+Prathmeshm1206@users.noreply.github.com>
2023-12-06 18:41:43 +0000

    dates

--- 01_basics/07_datesin_js.js
+++ 01_basics/07_datesin_js.js
@@ -10,5 +10,19 @@ let myCreateDate = new Date(2023, 0, 23, 5, 3)
 //console.log(myCreateDate.toDateString());  // Mon Jan 23 2023
 console.log(myCreateDate.toLocaleDateString()); // 1/23/2023
 
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);            // 1701886981689 milisec
+// console.log(myCreateDate.getTime()); // 1674450180000 milisec
+
+//console.log(Math.round(Date.now()/1000));
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getDay());
+console.log(newDate.getMonth());
+
+// newDate.toLocaleString('default',{
+//     weekday: "long"
+// })
 
 
