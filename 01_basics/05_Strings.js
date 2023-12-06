1e9fd0115ad528bee7137dd9bdefef38b7e388ed
Prathmeshm1206 <144794295+Prathmeshm1206@users.noreply.github.com>
2023-12-06 12:37:28 +0000

    strings

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,25 @@
+const name = "hitesh"
+const repoCount = 50
+
+console.log(name + repoCount + "value");  
+
+console.log(`Hello my name is ${name} and my repo is ${repoCount}`);
+console.log(`hey myself ${name} and my repo count is ${repoCount} `);
+
+const gameName = new String('hiteshhhc')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__); // contains objects
+
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4) //cannot give -ve value
+console.log(newString);
+
+
+const anotherString = gameName.slice(-8,4) // In this we can put negative value also ans it works reversly
+console.log(anotherString)
