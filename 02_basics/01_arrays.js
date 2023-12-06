8319d455c641b8cfb8fbc97e761b69702d638e9e
Prathmeshm1206 <144794295+Prathmeshm1206@users.noreply.github.com>
2023-12-06 19:01:38 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+// arrays
+
+const myArr = [0, 1, 2, 3, 4, 5,]
+const myHeros = ["abc", "xyz"]
+const myarr2 = new Array(1, 2, 3, 4)
+
+console.log(myArr[1]);
+
+// Arrays methods
+
+myArr.push(6)
+console.log(myArr);
+
