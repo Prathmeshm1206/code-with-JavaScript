0dbd51bb4e24fc914dc914bff938101ec0dc036e
Prathmeshm1206 <144794295+Prathmeshm1206@users.noreply.github.com>
2023-12-09 04:45:52 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -3,15 +3,15 @@ Object.create
 
 // to Create symbols
 
-const mysym = symbol("key1")
+//const mysym = symbol("key1")
 
 
 // object literals  ==> we can define from keys and values
 const JsUser ={
     name: "omkar",
     "Full name" : "Prathmesh mane",
-    [mysym]
-     : "mykey", //===> not working like symbol 
+    // [mysym]
+    //  : "mykey", //===> not working like symbol 
     age : 18,
     location: "Jaipur",
     isLoggedIn: false,
@@ -22,6 +22,15 @@ const JsUser ={
 console.log(JsUser.age);
 //console.log(JsUser[age]);
 console.log(JsUser["Full name"]);
-console.log(JsUser[mysym]);
+//Object.freeze(JsUser)   // cannot update further
+JsUser.email = "mane@gmail.com"
+console.log(JsUser.email);
 
+JsUser.greeing = function(){
+    console.log("Hello js user");
+}
+JsUser.greeingTwo = function(){
+    console.log(`Hello Js User ${this["Full name"]}`); //show all JsUser object properties
+}
 
+console.log(JsUser.greeing());  // => undefined  ()=>return funtion
