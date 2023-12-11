3826dd834c58ad9494106ae7d425b9b76ec355b0
Prathmeshm1206 <144794295+Prathmeshm1206@users.noreply.github.com>
2023-12-11 17:02:40 +0000

    scopes

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,18 @@
+
+let c = 300
+const b = 50
+if(true){
+    let a = 10
+    const b = 20
+    // var c = 30
+    
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+
+// console.log(a);
+console.log(b);
+console.log(c);
