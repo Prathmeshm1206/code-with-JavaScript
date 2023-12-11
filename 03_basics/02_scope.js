668c7b818147d396f73772619d7fe4bd70acfe4e
Prathmeshm1206 <144794295+Prathmeshm1206@users.noreply.github.com>
2023-12-11 17:44:28 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -5,14 +5,52 @@ if(true){
     let a = 10
     const b = 20
     // var c = 30
-    
+    //console.log("inner ",a);
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    
-}
+
 
 // console.log(a);
-console.log(b);
-console.log(c);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "hitesh"
+
+
+    // nested function
+    // => inner function two can access outer function (one)
+    // it is also called closure
+    function two (){
+        const website ="youtube";
+        console.log(username);
+    }
+    //console.log(website);  // error => outside the scope
+    two()
+}
+// one()
+
+if(true){
+    const username="hitesh"
+    if(username==="hitesh"){
+        const website ="youtube"
+        console.log(username + website);
+    }
+    // console.log(website);  error => out of scope 
+}
+// console.log(username); error => out of scope
+
+
+//+++++++++++++++++++++++++ Interesting ++++++++++++++++++++
+
+console.log(addone(5));
+function addone(num){
+   return num +1
+}
+
+// addTwo(5) ==> Hoisting  we cannot declear before funtion
+const addTwo = function (num) {
+    return num +2
+} 
+
+
