bc1e586840b8e2e1eb48af97b993e46896f0a56b
Prathmeshm1206 <144794295+Prathmeshm1206@users.noreply.github.com>
2023-12-12 05:00:33 +0000

    filter-map

---
+++ 05_iteration/four.js
@@ -0,0 +1,27 @@
+//const  coding = ["js","ruby", "java", "python", "cpp"]
+
+// const values = coding.forEach( (item) =>  {  //foreach loop cannot return values
+//  //   console.log(item);
+
+//     return item
+// })
+
+// console.log(values);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+
+//=>> it takes call back
+// const newNum =myNums.filter((num)=> {
+//  return num>4
+// })
+
+// const newNum =[]
+
+// myNums.forEach( (num)=> {
+//     if (num>4) {
+//         newNum.push(num)
+//     }
+// })
+// console.log(newNum);
+
